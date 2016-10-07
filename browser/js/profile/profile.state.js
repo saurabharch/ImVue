514d76f19520f4c280044d24e9bae3a0929cfb5d
Joe Bosman <joseph.a.bosman@gmail.com>
2016-10-07 13:04:27 -0400

    Resolved gulp warnings from master merge

--- browser/js/profile/profile.state.js
+++ browser/js/profile/profile.state.js
@@ -6,32 +6,10 @@ app.config(function ($stateProvider) {
         url: '/profile',
         templateUrl: 'js/profile/profile.html',
         controller: 'profileCtrl',
-        resolve:{
-            profileInfo: function(){
+        resolve: {
+            profileInfo: function(profileFactory){
                 return profileFactory.fetchProfileInfo();
             }
         }
     });
 });
-
-
-
-
-// 'use strict';
-//
-// app.config(function ($stateProvider) {
-//     $stateProvider.state('stories', {
-//         url: '/stories',
-//         templateUrl: '/browser/app/story/list/story.list.html',
-//         controller: 'StoryListCtrl',
-//         resolve: {
-//             stories: function (Story) {
-//                 return Story.fetchAll();
-//             },
-//             users: function (User) {
-//                 return User.fetchAll();
-//             }
-//         }
-//     });
-// });
-
