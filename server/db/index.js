a2e20e6663ac25836cc54a6c3fea4e7f90701885
Joe Bosman <joseph.a.bosman@gmail.com>
2016-10-13 12:27:04 -0400

    Committing to work on something else...

--- server/db/index.js
+++ server/db/index.js
@@ -5,23 +5,34 @@ const Location = require('./models/location');
 const Drawing = require('./models/drawing');
 const Text = require('./models/text');
 const Image = require('./models/image');
+const Project = require('./models/project')
 
 module.exports = db;
 
-Location.hasMany(Drawing);
-Location.hasMany(Text);
-Location.hasMany(Image);
+Project.hasOne(User);
+Project.hasOne(Location);
+Project.hasOne(Drawing);
+Project.hasMany(Text);
+Project.hasMany(Image);
+
+Location.belongsTo(Project);
+Drawing.belongsTo(Project);
+Text.belongsToMany(Project);
+Image.belongsToMany(Project);
+
+User.hasmany(Project);
+
 // Location.hasMany(User, {as: 'author'});
 
-Drawing.belongsTo(Location);
-Text.belongsTo(Location);
-Image.belongsTo(Location);
+// Drawing.belongsTo(Location);
+// Text.belongsTo(Location);
+// Image.belongsTo(Location);
 
 
-Drawing.belongsTo(User);
-Text.belongsTo(User);
-Image.belongsTo(User);
+// Drawing.belongsTo(User);
+// Text.belongsTo(User);
+// Image.belongsTo(User);
 
-User.hasMany(Drawing);
-User.hasMany(Text);
-User.hasMany(Image);
+// User.hasMany(Drawing);
+// User.hasMany(Text);
+// User.hasMany(Image);
---
+++ server/db/models/project.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var Sequelize = require('sequelize');
+
+var db = require('../_db');
+
+module.exports = db.define('project', {});
